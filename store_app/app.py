1fa6a28b86fe3a2ac69d5979b70f00ffef7c0764
Jon Welker <JWelker110@gmail.com>
2016-03-25 17:24:14 -0500

    Initial setup and configuration of the application

--- store_app/app.py
+++ store_app/app.py
@@ -1,5 +1,7 @@
 from flask import Flask
-from extensions import db
+from extensions import db, mail, bcrypt
+from config import DevelopmentConfig
+from database import User, Category, Item
 
 DEFAULT_BLUEPRINTS = ()
 
@@ -7,12 +9,37 @@ DEFAULT_BLUEPRINTS = ()
 def create_app(app_name=__name__, blueprints=DEFAULT_BLUEPRINTS, config=DevelopmentConfig):
     # create and set up the application
     app = Flask(app_name)
-    app.config.from_object(config)
+    app.config.from_object(DevelopmentConfig)
+    app.config.from_pyfile('config_secret.py')
     configure_extensions(app)
 
     return app
 
 
+def configure_processors(app):
+
+    @app.after_request
+    def after_request(response):
+        # TODO would be changed in prod
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        response.headers.add('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE')
+        return response
+
+    @app.error_handler(404)
+    def error_handler(error):
+        pass
+
+
 def configure_extensions(app):
     # setup SQLAlchemy
     db.init_app(app)
+    db.create_all(app=app)
+
+    mail.init_app(app)
+
+    bcrypt.init_app(app)
+
+
+def configure_blueprints(app, blueprints):
+    for bp in blueprints:
+        app.register_blueprint(bp)
