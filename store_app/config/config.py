80f446e3576e34cb4e7f5f2cf11d61024e472871
Jon Welker <JWelker110@gmail.com>
2016-03-25 18:08:02 -0500

    Update app vars

--- store_app/config/config.py
+++ store_app/config/config.py
@@ -1,7 +1,7 @@
 import os
 
 _absolute_path = os.path.dirname(os.path.abspath(__file__))
-_config_path = os.path.join(_absolute_path, 'config_secret.json')
+_config_path = os.path.join(_absolute_path, 'config_secret.py')
 
 
 class BaseConfig(object):
@@ -10,7 +10,9 @@ class BaseConfig(object):
     STATIC_PATH = os.path.join(PROJECT_ROOT, 'static')
     TEMPLATE_PATH = os.path.join(PROJECT_ROOT, 'templates')
     UPLOAD_PATH = os.path.join(PROJECT_ROOT, 'uploads')
+    CONFIG_PATH = _config_path
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
     TESTING = False
 
