6f96b9703f9200dff0adee2749db20c2c5652aaa
Jon Welker <JWelker110@gmail.com>
2016-04-02 17:04:46 -0500

    Add user JSON endpoints

--- store_app/blueprints/user.py
+++ store_app/blueprints/user.py
@@ -1,32 +1,52 @@
 from flask import Blueprint
+from json import dumps
+from string import lower
+
+from store_app.database import User, Item, Category
+from helpers import Resp
 
 user_bp = Blueprint("user_bp", __name__)
 
 
 @user_bp.route('/users', methods=['GET'])
-def users():
-    return "Users"
+def getUsers():
+    users = User.query.limit(10)
+    return Resp(response=dumps({
+        "users": [u.dict() for u in users]
+    }))
 
 
 @user_bp.route('/users/<int:offset>', methods=['GET'])
-def usersByOffset(offset):
-    return "The offset is: %s" % offset
+def getUsersByOffset(offset):
+    users = User.query.offset(offset).limit(10)
+    return Resp(response=dumps({
+        "users": [u.dict() for u in users]
+    }))
 
 
 @user_bp.route('/user/<string:username>', methods=['GET'])
-def userByUsername(username):
-    return "The username is: %s" % username
+def getUserByUsername(username):
+    user = User.query.filter_by(username_lower=lower(username)).limit(10)
+    return Resp(response=dumps({
+        "users": [u.dict() for u in user]
+    }))
 
 
 @user_bp.route('/user/<string:username>/items', methods=['GET'])
-def itemsByUsername(username):
-    return "The username is: %s" % username
+def getItemsByUsername(username):
+    items = Item.query.filter_by(owner_name=lower(username)).limit(10)
+    return Resp(response=dumps({
+        "items": [i.dict() for i in items]
+    }))
 
 
 @user_bp.route('/user/<string:username>/items/<int:offset>', methods=['GET'])
-def itemsByUsernameOffset(username, offset):
-    return "The username is: %s and the offset is: %s" % (username, offset)
+def getItemsByUsernameOffset(username, offset):
+    items = Item.query.filter_by(owner_name=lower(username)).offset(offset).limit(10)
+    return Resp(response=dumps({
+        "items": [i.dict() for i in items]
+    }))
 
 @user_bp.route('/user', methods=['POST'])
-def user():
+def getUser():
     pass
