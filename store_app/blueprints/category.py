fac874cb8087f07b9b5fc2dc4ad8db0fb493c2ea
Jon Welker <JWelker110@gmail.com>
2016-04-03 17:05:45 -0500

    Add the first few endpoints

--- store_app/blueprints/category.py
+++ store_app/blueprints/category.py
@@ -1,3 +1,25 @@
 from flask import Blueprint
 
+from store_app.database import Category, Item, User, CategoryItems
+from helpers import Resp, simple_enc
+from store_app.extensions import db
+
 category_bp = Blueprint('category_bp', __name__)
+
+
+@category_bp.route('/categories', methods=['GET'])
+def getCategories():
+    cats = Category.query.all()
+    return Resp(simple_enc("categories", cats))
+
+
+@category_bp.route('/category/<string:category>/items')
+def getItemsByCategory(category):
+    items = db.session.query(Item).outerjoin(CategoryItems).outerjoin(Category).filter_by(name=category).limit(10)
+    return Resp(simple_enc("items", items))
+
+
+@category_bp.route('/category/<string:category>/items/<int:offset>')
+def getItemsByCategoryOffset(category, offset):
+    items = db.session.query(Item).outerjoin(CategoryItems).outerjoin(Category).filter_by(name=category).offset(offset).limit(10)
+    return Resp(simple_enc("items", items))
