7320905437d22bb19ca95fd14e4e355bdad73437
Jon Welker <JWelker110@gmail.com>
2016-04-09 18:59:31 -0500

    Update confirm email sender to return success

--- store_app/blueprints/helpers/mailer.py
+++ store_app/blueprints/helpers/mailer.py
@@ -1,4 +1,3 @@
-from flask import render_template
 from flask_mail import Message
 
 from store_app.extensions import mail
@@ -24,6 +23,4 @@ def send_confirmation_email(to, token, subject="Please confirm your email"):
     template = "<p>To begin using your account, please <a href='http://localhost:8080/confirm?token=" \
                + token + "'>confirm your email</a></p>"
 
-    send_email(to, subject, template)
-
-send_confirmation_email('jwelker110@gmail.com', '12345lolol')
+    return send_email(to, subject, template)
