ad5f83c2813cbb38538e23e75ff1c782f5575d95
Jon Welker <JWelker110@gmail.com>
2016-04-09 19:03:49 -0500

    Add jwt create and access functions

--- store_app/blueprints/helpers/authentication.py
+++ store_app/blueprints/helpers/authentication.py
@@ -1,13 +1,15 @@
 from itsdangerous import URLSafeTimedSerializer, BadSignature
 import random
 import string
+import os
+import jwt
 
 
 def generate_secret_key():
     return ''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(15))
 
 
-def generate_confirm_token(email, secretKey):
+def generate_token(email, secretKey):
 
     serializer = URLSafeTimedSerializer(secret_key=secretKey)
     return serializer.dumps(email)
@@ -25,3 +27,12 @@ def confirm_token(token, secretKey):
         return False
     return email
 
+
+def create_jwt(payload):
+    key = os.environ.get('JWT_CIPHER')
+    return None if key is None else jwt.encode(payload, key, algorithm='HS256')
+
+
+def decode_jwt(jwt_token):
+    key = os.environ.get('JWT_CIPHER')
+    return None if key is None else jwt.decode(jwt_token, key=key, algorithms=['HS256'])
