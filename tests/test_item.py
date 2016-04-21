e6c0db91b8ca51ee2f6bb4144e66b7123e63dee3
Jon Welker <JWelker110@gmail.com>
2016-04-20 21:48:23 -0500

    Add tests for items that don't exist and offsets too high

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,6 +1,9 @@
 from base import StoreAppTestCase
 from json import loads, dumps
 
+from store_app.blueprints import decode_jwt
+from store_app.database import Item, ItemMeta
+
 
 class TestItem(StoreAppTestCase):
 
@@ -20,17 +23,35 @@ class TestItem(StoreAppTestCase):
         items = data.get('items')
         self.assertEqual(12, len(items), 'Expected 12 items, retrieved %s.' % str(len(items)))
 
+    def test_getItemsByOffsetTooHigh(self):
+        req = self.client.get('/api/v1/items.json?offset=99999999999')
+        data = loads(req.data)
+        items = data.get('items')
+        self.assertEqual(20, len(items), 'Expected 20 items, retrieved %s.' % str(len(items)))
+
     def test_getItemsByCategory(self):
         req = self.client.get('/api/v1/items.json?category=Category1')
         data = loads(req.data)
         items = data.get('items')
         self.assertEqual(2, len(items), 'Expected 2 items in \'Category1\', retrieved %s.' % str(len(items)))
 
+    def test_getItemsByCategoryNotExist(self):
+        req = self.client.get('/api/v1/items.json?category=CategoryHerpDerp')
+        data = loads(req.data)
+        items = data.get('items')
+        self.assertEqual(0, len(items), 'Expected 0 items in \'CategoryHerpDerp\', retrieved %s.' % str(len(items)))
+
     def test_getItemsByCategoryOffset(self):
         req = self.client.get('/api/v1/items.json?category=Category1&offset=1')
         data = loads(req.data)
         items = data.get('items')
         self.assertEqual(1, len(items), 'Expected 1 item in \'Category1\' when offset by 1, retrieved %s' % str(len(items)))
+
+    def test_getItemsByCategoryNotExistOffsetTooHigh(self):
+        req = self.client.get('/api/v1/items.json?category=CategoryHerpDerp&offset=9999999999')
+        data = loads(req.data)
+        items = data.get('items')
+        self.assertEqual(0, len(items), 'Expected 0 items in \'CategoryHerpDerp\', offset too high, retrieved %s.' % str(len(items)))
 
     def test_getItemInfo(self):
         req = self.client.get('/api/v1/items/details.json?name=Item1')
@@ -58,4 +79,28 @@ class TestItem(StoreAppTestCase):
             self.assertIn('meta_value', meta, 'ItemMeta value not found in response.')
 
     def test_createItem(self):
-        pass
+        # first login and retrieve the jwt
+        req = self.client.post('/login', data=dumps({
+            'username': 'Tester1',
+            'password': 'lol123'
+        }))
+        data = loads(req.data)
+        jwt_token = data.get('jwt_token')
+
+        req = self.client.post('/api/v1/items.json', data=dumps({
+            "jwt_token": jwt_token,
+            "name": "Item123",
+            "description": "This is the best item ever",
+            "price": 2596,
+            "sale_price": 1999,
+            "stock": 1337,
+            "meta_description": "This is the default item description",
+            "meta_key": "Color",
+            "meta_value": "Black"
+        }))
+        self.assertEqual(req.status, '200 OK', "Creating an item returned status %s" % str(req.status))
+
+        item = Item.query.filter_by(name='Item123').first()
+        self.assertIsNotNone(item, 'Item123 was not created.')
+        itemMeta = ItemMeta.query.filter_by(item_id=item.id).first()
+        self.assertIsNotNone(itemMeta, 'Item123 Meta was not created.')
