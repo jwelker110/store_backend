b5ea2ac2e6263fd139f896d1b0229c5e2dab5c4e
Jon Welker <JWelker110@gmail.com>
2016-05-21 13:31:26 -0500

    Remove mail and bcrypt extensions

--- store_app/extensions.py
+++ store_app/extensions.py
@@ -1,9 +1,3 @@
 from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 db.make_declarative_base()
-
-from flask_mail import Mail
-mail = Mail()
-
-from flask_bcrypt import Bcrypt
-bcrypt = Bcrypt()
