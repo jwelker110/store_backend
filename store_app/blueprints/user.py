cf3dd3c6f6ca7881e75c7a9144600ddaa7db7d13
Jon Welker <JWelker110@gmail.com>
2016-05-21 13:10:56 -0500

    Add endpoint to check whether user exists already

--- store_app/blueprints/user.py
+++ store_app/blueprints/user.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from string import lower
 from jwt import encode, decode
+from sqlalchemy import desc
 
 from store_app.database import User, Item, Category
 from helpers import create_response, convertToInt
@@ -11,7 +12,7 @@ user_bp = Blueprint("user_bp", __name__)
 @user_bp.route('/api/v1/users.json', methods=['GET'])
 def users_ep():
     offset = convertToInt(request.args.get('offset'))
-    users = User.query.offset(offset).limit(25).all()
+    users = User.query.offset(offset).limit(20).all()
 
     return create_response({"users": users})
 
@@ -24,6 +25,20 @@ def users_items_ep():
     if username is None:
         items = []
     else:
-        items = Item.query.filter_by(owner_name=lower(username)).offset(offset).limit(25).all()
+        items = Item.query.filter_by(owner_name=lower(username)).order_by(desc(Item.id)).offset(offset).limit(20).all()
 
     return create_response({"items": items})
+
+
+@user_bp.route('/api/v1/users/exists.json', methods=['GET'])
+def users_exists_ep():
+    username = request.args.get('username')
+
+    if username is None:
+        return create_response({}, status=400)
+
+    user = User.query.filter_by(username_lower=lower(username)).first()
+    if user is None:
+        return create_response({"exists": False})
+
+    return create_response({"exists": True})
