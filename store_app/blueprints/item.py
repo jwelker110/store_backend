5cbee881c76d340676f5052d0c7b250090856cea
Jon Welker <JWelker110@gmail.com>
2016-05-21 13:09:52 -0500

    Add ability to delete your created items

--- store_app/blueprints/item.py
+++ store_app/blueprints/item.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request
 from json import loads
 from string import lower
+from sqlalchemy import desc
 
 from store_app.database import Item, CategoryItems, Category
 from store_app.extensions import db
@@ -19,12 +20,10 @@ def items_ep():
         category = request.args.get('category')
 
         if category is None:
-            items = Item.query.offset(offset).limit(25).all()
+            items = Item.query.order_by(desc(Item.id)).offset(offset).limit(10).all()
         else:
-            items = db.session.query(Item)\
-                .outerjoin(CategoryItems)\
-                .outerjoin(Category)\
-                .filter_by(name=category).offset(offset).limit(25).all()
+            items = db.session.query(Item).order_by(desc(Item.id))\
+                .filter_by(name=category).offset(offset).limit(10).all()
 
         return create_response({"items": items})
 
@@ -35,9 +34,12 @@ def items_ep():
         # item info
         name = data.get('name')
         description = data.get('description')
+        category_id = data.get('category_id')
         price = data.get('price')
         sale_price = data.get('sale_price')
         image_url = data.get('image_url')
+        if image_url is None:
+            image_url = 'uploads/image.jpg'
         stock = data.get('stock')
 
         # jwt to ensure user is authorized
@@ -49,7 +51,7 @@ def items_ep():
         if payload.get('confirmed') is False:
             return create_response({}, status=401)
 
-        if name is None or price is None:
+        if name is None or price is None or category_id is None:
             return create_response({}, status=400)
 
         try:
@@ -66,6 +68,14 @@ def items_ep():
             db.session.add(item)
             db.session.commit()
 
+            catItem = CategoryItems(
+                category_id=category_id,
+                item_id=item.id
+            )
+
+            db.session.add(catItem)
+            db.session.commit()
+
             return create_response({})
 
         except:
@@ -73,7 +83,7 @@ def items_ep():
             return create_response({}, status=500)
 
 
-@item_bp.route('/api/v1/items/details.json', methods=['GET', 'PUT'])
+@item_bp.route('/api/v1/items/details.json', methods=['GET', 'PUT', 'POST'])
 def item_details_ep():
 
     # todo this needs to return a JSON object representing the item with the 
@@ -88,16 +98,41 @@ def item_details_ep():
         # tuples are returned from the query so must be accessed via index
         return create_response({"item": item})
 
+    elif request.method == 'POST':
+        data = loads(request.data)
+        payload = decode_jwt(data.get('jwt_token'))
+        itemName = data.get('name')
+
+        if payload is None:
+            return create_response({}, status=401)
+
+        item = Item.query.filter_by(
+            name=itemName, 
+            owner_name=lower(payload.get('username'))).first()
+
+        if item is None:
+            return create_response({}, status=400)
+
+        try:
+            db.session.delete(item)
+            db.session.commit()
+            return create_response({})
+        except:        
+            db.session.rollback()
+            return create_response({}, status=500)
+
     elif request.method == 'PUT':
 
         data = loads(request.data)
 
         # item info
+        itemId = data.get('id')
         name = data.get('name')
         description = data.get('description')
         image_url = data.get('image_url')
         price = data.get('price')
         sale_price = data.get('sale_price')
+        image_url = data.get('image_url')
         stock = data.get('stock')
 
         payload = decode_jwt(data.get('jwt_token'))
@@ -106,7 +141,7 @@ def item_details_ep():
             return create_response({}, status=401)
 
         # get the item
-        item = Item.query.filter_by(name=name).first()
+        item = Item.query.filter_by(id=itemId).first()
 
         # does the item exist? how about the item meta?
         if item is None:
@@ -125,6 +160,7 @@ def item_details_ep():
             item.image_url = item.image_url if image_url is None else image_url
             item.price = item.price if price is None else price
             item.sale_price = item.sale_price if sale_price is None else sale_price
+            item.image_url = item.image_url if image_url is None else image_url
             item.stock = item.stock if stock is None else stock
 
             db.session.commit()
