ba2e2f0535b6ddae7c0fce811f84ca710839fc00
Jon Welker <JWelker110@gmail.com>
2016-05-21 13:12:37 -0500

    Add category reference within item table

--- store_app/database/schema/item.py
+++ store_app/database/schema/item.py
@@ -7,18 +7,20 @@ class Item(db.Model):
     __tablename__ = 'Item'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(250), nullable=False)
+    name = db.Column(db.String(250), nullable=False, unique=True)
     description = db.Column(db.String(500))
+    category = db.Column(db.String(50), db.ForeignKey("Category.name"), nullable=False)
     owner_name = db.Column(db.String(20), db.ForeignKey("User.username_lower"), nullable=False)
     image_url = db.Column(db.String(25))
-    price = db.Column(db.Numeric)
+    price = db.Column(db.Numeric, nullable=False)
     sale_price = db.Column(db.Numeric)
     stock = db.Column(db.Integer)
     created_on = db.Column(db.DateTime)
 
-    def __init__(self, owner_name, name=None, description=None, image_url=None, price=None, sale_price=None, stock=None):
+    def __init__(self, owner_name, name=None, description=None, category="Other", image_url=None, price=None, sale_price=None, stock=None):
         self.name = name
         self.description = description
+        self.category = category
         self.owner_name = lower(owner_name)
         self.image_url = image_url
         self.price = price
@@ -34,6 +36,7 @@ class Item(db.Model):
             "id": self.id,
             "name": self.name,
             "description": self.description,
+            "category": self.category,
             "owner_name": self.owner_name,
             "image_url": self.image_url,
             "price": self.price,
