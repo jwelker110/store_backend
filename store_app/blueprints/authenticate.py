aa5d7df01c9fb44977ddb592269b46eb875517a7
Jon Welker <JWelker110@gmail.com>
2016-05-21 13:08:45 -0500

    Add oauth to jwt and allow user to use Google Oauth to signup/in

--- store_app/blueprints/authenticate.py
+++ store_app/blueprints/authenticate.py
@@ -25,12 +25,7 @@ def register():
     email = req.get('email')
     username = req.get('username')
     password = req.get('password')
-    recaptchaResponse = req.get('g-recaptcha-response')
-
-    # the data to be returned to the client
-    data = {
-        "jwt_token": None
-    }
+    recaptchaResponse = req.get('g_recaptcha_response')
 
     # todo uncomment when no longer testing
     # let's verify the user passed the recaptcha
@@ -41,11 +36,11 @@ def register():
     # if not resp['success']:  # they must be a robot or something
     #     return create_response({}, status=403)
 
-    if email is None or username is None:
+    if email is None or password is None:
         return create_response({}, status=400)
 
     # check if the user already exists
-    user = User.query.filter_by(username_lower=lower(username)).first()
+    user = User.query.filter_by(email_lower=lower(email)).first()
     if user is not None:  # todo maybe I should return error messages?
         return create_response({}, status=409)
 
@@ -63,19 +58,23 @@ def register():
 
         payload = {
             "username": user.username,
+            "isOauth": False,
+            "avatarUrl": user.avatar_url,
             "confirmed": user.confirmed
         }
-        jwt_token = create_jwt(payload)
-        data['jwt_token'] = jwt_token
 
         # creates a confirmation email for the user based on their email and secret
         token = generate_token(user.email, user.confirm_secret)
 
         if send_confirmation_email(to=user.email, token=token):
             # the email was sent, let the user know
-            return create_response(data)
+            return create_response({
+                "jwt_token": create_jwt(payload)
+                })
 
-        return create_response({}, status=500)
+        return create_response({
+            "jwt_token": create_jwt(payload)
+            }, status=500)
 
     except Exception as e:
         db.session.rollback()
@@ -89,13 +88,13 @@ def login():
     :return: JWT with the claims for the user to be sent on future requests
     """
     req = loads(request.data)
-    username = req.get('username')
+    email = req.get('email')
     password = req.get('password')
 
-    if username is None or password is None:
+    if email is None or password is None:
         return create_response({}, status=400)
 
-    user = User.query.filter_by(username_lower=lower(username)).first()
+    user = User.query.filter_by(email_lower=lower(email)).first()
 
     if user is None:
         return create_response({}, status=400)
@@ -103,6 +102,8 @@ def login():
     if user.check_password_hash(password):
         payload = {
             "username": user.username,
+            "isOauth": False,
+            "avatarUrl": user.avatar_url,
             "confirmed": user.confirmed
         }
         return create_response({
@@ -160,42 +161,32 @@ def jwt_reauth():
     return create_response({'jwt_token': create_jwt(payload)})
 
 
-@auth_bp.route('/goauth', methods=['GET'])
+@auth_bp.route('/goauth', methods=['POST'])
 def google_oauth():
     """
-    Executes the auth flow required for google auth. If the user already has an account, they
-    are signed in. If the user does not have an account, one is created for them and their account
-    is automatically confirmed.
+    If the user already has an account, they are signed in. If the user does 
+    not have an account, one is created for them and their account is 
+    automatically confirmed.
     :return: JWT with the claims for the user to be sent on future requests
     """
-    flow = client.flow_from_clientsecrets(
-        filename='store_app/blueprints/helpers/client_secrets.json',  # todo add these to environ var?
-        scope='profile email',
-        redirect_uri=url_for('auth_bp.google_oauth', _external=True)
-    )
 
-    if 'code' not in request.args:
-        auth_uri = flow.step1_get_authorize_url()
-        return redirect(auth_uri)  # todo change this
-        # return create_response({'uri': auth_uri})
-    auth_code = request.args.get('code')
-    credentials = flow.step2_exchange(auth_code)
-    http_auth = credentials.authorize(httplib2.Http())
-    resp = http_auth.request('https://www.googleapis.com/oauth2/v2/userinfo')
-    userinfo = loads(resp[1])
+    req = loads(request.data)
+    email = req.get('email')
+    oa_id = req.get('oa_id')
+
+    if email is None or oa_id is None:
+        return create_response({}, status=400)
 
     # if the account already exists, sign in, else create the account and sign in.
-    user = User.query.filter_by(email_lower=userinfo.get('email')).first()
+    user = User.query.filter_by(email_lower=lower(email)).first()
     if user is None:
 
         try:
             # create the account
             user = User(
-                oa_id=userinfo.get('id'),
-                first_name=userinfo.get('given_name'),
-                last_name=userinfo.get('family_name'),
-                email=userinfo.get('email'),
-                username=split(userinfo.get('email'), '@')[0],
+                oa_id=oa_id,
+                email=email,
+                username=split(email, '@')[0],
                 confirmed=True
             )
             db.session.add(user)
@@ -207,6 +198,8 @@ def google_oauth():
 
     payload = {
         "username": user.username,
+        "isOauth": True,
+        "avatarUrl": user.avatar_url,
         "confirmed": user.confirmed
     }
     return create_response({
