9a702e557e4cb071e66087e5db55a70d964531ec
Jon Welker <JWelker110@gmail.com>
2016-05-28 16:55:19 -0500

    Category Info
    Add lower name to category

--- store_app/database/schema/category.py
+++ store_app/database/schema/category.py
@@ -1,3 +1,5 @@
+from string import lower
+
 from store_app.extensions import db
 
 
@@ -5,10 +7,12 @@ class Category(db.Model):
     __tablename__ = 'Category'
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    name_lower = db.Column(db.String(50), nullable=False, unique=True)
 
-    def __init__(self, name=None):
+    def __init__(self, name='Other'):
         self.name = name
+        self.name_lower = lower(name)
 
     def __repr__(self):
         return "<Category(%s)>" % self.name
