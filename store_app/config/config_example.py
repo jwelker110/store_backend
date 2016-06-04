c74fab476d0a36c72b0a70997a61a1a8bb5f3464
Jon Welker <JWelker110@gmail.com>
2016-06-04 18:39:52 -0500

    Update example so that it can be easily used after renamed to config_secret

--- store_app/config/config_example.py
+++ store_app/config/config_example.py
@@ -1,13 +1,7 @@
 # replace the contents of this file with the required information and rename the file to config_secret.py
 DB_USERNAME = "user",
 DB_PASSWORD = "password",
-DB_NAME = "db_name",
-MAIL_USERNAME = "email@gmail.com",
-MAIL_PASSWORD = "password",
-MAIL_PORT = 485,
-MAIL_SERVER = "smtp.gmail.com",
+DB_NAME = "store_app",
 SECRET_KEY = "secret key",
-ADMINS = ["email@gmail.com"],
 BCRYPT_LOG_ROUNDS = 12,
-JWT_CIPHER_KEY = "cipher key",
-RECAPTCHA_SECRET = "recaptcha secret"
+JWT_CIPHER_KEY = "cipher key"
